f7c3597ba4395659417be42d939cf7dd2027a132
iustinPetrisor <iustin.petrisor@vuzum.com>
2014-06-25 18:05:19 +0300

    dada

---
+++ slider.js
@@ -0,0 +1,119 @@
+var Slider = function ( wrapper , nav_bar, pageWidth, speedFadeOut, speedSlide, speedFadeIn) {
+	// body...
+
+	this._initialize(wrapper, pageWidth);
+
+	this.slider.children().css('max-width', pageWidth);
+	this.numberPages = this.slider.children().length;
+
+	this.totalWidth = this.pageWidth * this.numberPages;
+	this.slider.css('max-width', this.totalWidth);
+	this.nav_bar = nav_bar;
+	this.wrapper.css('max-width', this.pageWidth);
+	this.index = 0;	
+	this.speedFadeOut = speedFadeOut;
+	this.speedSlide = speedSlide;
+	this.speedFadeIn = speedFadeIn;
+
+}
+
+
+Slider.prototype.slide = function( direction ) {
+	this.direction = direction;
+	
+	this._fadeOut();
+
+};
+
+Slider.prototype._initialize = function (wrapper , pageWidth) {
+	
+	this.pageWidth = pageWidth;
+	this.wrapper = wrapper;
+	this.slider = this.wrapper.children('div.slider');
+	var length = this.slider.children().length;
+
+	this.slider.children().first().clone().insertAfter(this.slider.children().eq(length - 1));
+};
+
+Slider.prototype._update = function() {
+
+	if(this.direction === 'prev') {
+
+		if (this.index === 0) {
+			this.slider.css('marginLeft', - (this.numberPages-1) * this.pageWidth);
+			this._decrement();
+		};
+		this._decrement();
+	} else{
+		if (this.index === this.numberPages - 1 ) {
+			this.slider.css('marginLeft', 0);
+			this._increment();
+		};
+		this._increment();
+	}	
+};
+Slider.prototype._increment = function() {
+	// body...
+	this.index = (this.index + 1) % this.numberPages;
+};
+Slider.prototype._decrement = function() {
+
+	this.index--;
+	if (this.index < 0 ) this.index = this.numberPages - 1;
+};
+Slider.prototype._fadeOut = function() {
+	console.log(this.dir);
+
+	this.slider.children().eq(this.index).children().animate({
+		opacity: 0
+	}, this.speedFadeOut, this._slideTo.bind(this))
+};
+Slider.prototype._slideTo = function(  ) {
+	
+	this._update();
+	this.slider.animate({
+
+		marginLeft: - this.index * this.pageWidth
+	
+	}, this.speedSlide, this._fadeIn.bind(this))
+}
+Slider.prototype.slideTo = function( dir ) {
+	this.direction = dir;
+	this._update();
+	this.slider.animate({
+
+		marginLeft: - this.index * this.pageWidth
+	
+	}, this.speedSlide)
+}
+
+Slider.prototype._fadeIn = function() {
+	
+	this.slider.children().eq(this.index).children().animate({
+		opacity: 1
+	}, this.speedFadeIn)
+
+};
+
+
+
+
+getRandColor = function() {
+  var r = parseInt(Math.random() * 255),
+  g = parseInt(Math.random() * 255),
+  b = parseInt(Math.random() * 255);
+  return 'rgb('+ r +','+ g +','+ b +')';
+}
+
+
+
+
+
+
+
+
+// function selectProjects( element, attribute ) {
+// 	// this.element = element;
+// 	// this.attribute = attribute;
+// 	element.children.
+// }	
